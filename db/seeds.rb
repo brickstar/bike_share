d5126a1be42241ae702d2aad99543d943202c233
brickstar <brickstar@gmail.com>
2018-07-21 17:11:15 -0600

    fix seed path

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Trip.destroy_all
 Station.destroy_all
 Condition.destroy_all
 
-CSV.foreach('./db/sf-bay-area-bike-share/trip.csv', headers: true, header_converters: :symbol).with_index do |row, index|
+CSV.foreach('./../sf-bay-area-bike-share/trip.csv', headers: true, header_converters: :symbol).with_index do |row, index|
   if index%149 == 0
     formatted_start_date = DateTime.strptime row[2], '%m/%d/%Y %H:%M'
     formatted_end_date = DateTime.strptime row[2], '%m/%d/%Y %H:%M'
@@ -12,12 +12,12 @@ CSV.foreach('./db/sf-bay-area-bike-share/trip.csv', headers: true, header_conver
   end
 end
 
-CSV.foreach('./db/sf-bay-area-bike-share/station.csv', headers: true, header_converters: :symbol) do |row|
+CSV.foreach('./../sf-bay-area-bike-share/station.csv', headers: true, header_converters: :symbol) do |row|
   formatted_date = Date.strptime row[6], '%m/%d/%Y'
   Station.create(id: row[0], name:row[1], dock_count:row[4], city:row[5], installation_date:formatted_date)
 end
 
-CSV.foreach('./db/sf-bay-area-bike-share/weather.csv', headers: true, header_converters: :symbol).with_index do |row, index|
+CSV.foreach('./../sf-bay-area-bike-share/weather.csv', headers: true, header_converters: :symbol).with_index do |row, index|
   formatted_date = Date.strptime row[0], '%m/%d/%Y'
   Condition.create(date:formatted_date, max_temp:row[1], mean_temp:row[2], min_temp:row[3], mean_humidity:row[8], mean_visibility:row[14], mean_wind_speed:row[17], precipitation:row[19])
 end
