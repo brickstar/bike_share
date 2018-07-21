ce1c666f09b78305470045bf052cfbe8484e5258
JP Lynch <JPLynch35@gmail.com>
2018-07-21 13:18:51 -0600

    add tests for trips pagination buttons

--- spec/features/visitors/sees_all_trips_spec.rb
+++ spec/features/visitors/sees_all_trips_spec.rb
@@ -1,16 +1,29 @@
 require 'rails_helper'
 
 describe 'visiting trips index' do
-  it 'sees 30 trips per page' do
+  before :each do
     45.times do |index|
       Trip.create(duration: index, start_date: '12/12/2015 12:12', start_station_name: 'Test Station 1', start_station_id: 1, end_date: '12/12/2015 12:42', end_station_name: 'Test Station 2', end_station_id: 2, bike_id: 2, subscription_type: 'customer', zip_code: '32174')
     end
-
+  end
+  it 'sees 30 trips per page' do
     visit trips_path
 
     expect(page).to have_content('All Trips')
     expect(page).to have_content("Duration: #{Trip.first.duration}s")
     expect(page).to have_content("Duration: #{Trip.find(30).duration}s")
     expect(page).to_not have_content("Duration: #{Trip.find(31).duration}s")
+  end
+  it 'sees a button to click on and see more trips' do
+    visit trips_path
+
+    expect(page).to have_content('Next >')
+  end
+  it 'can go forward and back in trips on second trips page' do
+    visit trips_path
+    click_on 'Next >'
+    
+    expect(page).to have_content('Next >')
+    expect(page).to have_content('< Previous')
   end
 end
