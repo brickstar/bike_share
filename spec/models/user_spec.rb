ff096d6b9acc5020701bc86769ff272ccd5cc475
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-21 17:56:42 -0600

    create user model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:street)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:zip_code)}
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_presence_of(:password)}
+
+  end
+end
