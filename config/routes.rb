4f019c921b9e16c0252bd7a914661c5e30b51951
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-21 16:58:42 -0600

    add welcome controller, login and login paths, and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
   resources :stations
 
   resources :conditions, only: [:index]
 
   resources :trips, only: [:index, :show]
-  
+
   get '/:station_name', to: 'stations#show'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
 
 end
