8b3884b6f1257873d31f5d3931887e3a684a9b3f
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-21 12:10:55 -0600

    build out controller and view, test passing

--- spec/features/visitors/sees_all_stations_spec.rb
+++ spec/features/visitors/sees_all_stations_spec.rb
@@ -12,9 +12,9 @@ describe 'visiting the stations index' do
     station_1 = Station.create(name: 'San Jose City Hall', city: 'San Jose', dock_count: 15, installation_date: 8/6/2013)
     station_2 = Station.create(name: 'Mountain View City Hall', city: 'Mountain View', dock_count: 17, installation_date: 8/16/2014)
 
-    visit stations_index_path
+    visit stations_path
 
-    expect(current_path).to eq(stations_index)
+    expect(current_path).to eq(stations_path)
     expect(page).to have_content(station_1.name)
     expect(page).to have_content(station_2.name)
     expect(page).to have_content(station_1.dock_count)
