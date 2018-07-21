071f11232b56c15d47ad8867e5e5232d78f0a469
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-21 15:36:50 -0600

    build out views and controller to pass feature test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
   resources :stations
-  
+  resources :conditions, only: [:index]
 end
--- spec/features/visitors/sees_all_conditions_spec.rb
+++ spec/features/visitors/sees_all_conditions_spec.rb
@@ -9,27 +9,27 @@ I see all conditions (Date, Max Temperature, Mean Temperature, Min Temperature, 
 
 describe 'visiting the conditions index' do
   it 'sees a list of all conditions' do
-    condition_1 = Condition.create(date: '8/29/2013', max_temp: 74.0, mean_temp: 68.0, min_temp: 61.0, mean_humidity: 61.0, mean_visibility: 58.0 , mean_wind_speed: 56.0, precipitation: 93.0)
-    condition_2 = Condition.create(date: '2/24/2014', max_temp: 71.0, mean_temp: 58.0, min_temp: 46.0, mean_humidity: 50.0, mean_visibility: 45.0 , mean_wind_speed: 41.0, precipitation: 93.0)
+    condition_1 = Condition.create(date: 'Tue, 04 Aug 2015', max_temp: 74.0, mean_temp: 68.0, min_temp: 61.0, mean_humidity: 61.0, mean_visibility: 58.0 , mean_wind_speed: 56.0, precipitation: 93.0)
+    condition_2 = Condition.create(date: 'Tue, 04 Aug 2015', max_temp: 71.0, mean_temp: 58.0, min_temp: 46.0, mean_humidity: 50.0, mean_visibility: 45.0 , mean_wind_speed: 41.0, precipitation: 93.0)
 
     visit conditions_path
 
     expect(current_path).to eq(conditions_path)
-    expect(page).to have_content(station_1.date)
-    expect(page).to have_content(station_2.date)
-    expect(page).to have_content(station_1.max_temp)
-    expect(page).to have_content(station_2.max_temp)
-    expect(page).to have_content(station_1.mean_temp)
-    expect(page).to have_content(station_2.mean_temp)
-    expect(page).to have_content(station_1.min_temp)
-    expect(page).to have_content(station_2.min_temp)
-    expect(page).to have_content(station_1.mean_humidity)
-    expect(page).to have_content(station_2.mean_humidity)
-    expect(page).to have_content(station_1.mean_visibility)
-    expect(page).to have_content(station_2.mean_visibility)
-    expect(page).to have_content(station_1.mean_wind_speed)
-    expect(page).to have_content(station_2.mean_wind_speed)
-    expect(page).to have_content(station_1.precipitation)
-    expect(page).to have_content(station_2.precipitation)
+    expect(page).to have_content(condition_1.date)
+    expect(page).to have_content(condition_2.date)
+    expect(page).to have_content(condition_1.max_temp)
+    expect(page).to have_content(condition_2.max_temp)
+    expect(page).to have_content(condition_1.mean_temp)
+    expect(page).to have_content(condition_2.mean_temp)
+    expect(page).to have_content(condition_1.min_temp)
+    expect(page).to have_content(condition_2.min_temp)
+    expect(page).to have_content(condition_1.mean_humidity)
+    expect(page).to have_content(condition_2.mean_humidity)
+    expect(page).to have_content(condition_1.mean_visibility)
+    expect(page).to have_content(condition_2.mean_visibility)
+    expect(page).to have_content(condition_1.mean_wind_speed)
+    expect(page).to have_content(condition_2.mean_wind_speed)
+    expect(page).to have_content(condition_1.precipitation)
+    expect(page).to have_content(condition_2.precipitation)
   end
 end
