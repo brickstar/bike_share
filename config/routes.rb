7a0e2caa8b05c955874859c883b84cb4677fa573
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-21 17:44:03 -0600

    add create account button to session new view, add resources user to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
+  resources :users
+
   resources :stations
 
   resources :conditions, only: [:index, :show]
