a009368c9b75828038ba83a097db31739b2c709d
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-21 18:55:15 -0600

    add create and show methods to user controller along with params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
 
-  def new 
+  def new
+    @user = User.new
   end
-end
+
+ def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :street, :city, :state, :zip_code, :email, :password)
+    end
+  end
