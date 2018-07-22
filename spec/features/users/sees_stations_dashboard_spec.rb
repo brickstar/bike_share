bd769406e47e950d77c67320c616d5132f215cd4
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-22 12:26:43 -0600

    add feature test for stations dashboard, skip all before heading to model level spec to build methods

--- spec/features/users/sees_stations_dashboard_spec.rb
+++ spec/features/users/sees_stations_dashboard_spec.rb
@@ -12,27 +12,41 @@ require 'rails_helper'
 # I also see the Oldest station.
 
 describe 'visiting the /stations-dashboard' do
-  it 'sees a total count of stations' do
+  xbefore :each do
+    station_1 = Station.create(name: 'San Jose City Hall', city: 'San Jose', dock_count: 15, installation_date: 8/6/2013)
+    station_2 = Station.create(name: 'Mountain View City Hall', city: 'Mountain View', dock_count: 17, installation_date: 8/16/2014)
+    station_3 = Station.create(name: 'Market at 10th', city: 'San Francisco', dock_count: 27, installation_date: 1/23/2013)
   end
 
-  it 'sees average bikes available per station' do
+  xit 'sees a total count of stations' do
+    expect(page).to have_content("Total Number of Stations: #{Station.total_station_count}")
   end
 
-  it 'sees most bikes available at a station' do
+  xit 'sees average bikes available per station' do
+    expect(page).to have_content("Average Bikes Available per Station: #{Station.avg_bikes_available}")
   end
 
-  it 'sees the station that has the most bikes available' do
+  xit 'sees most bikes available at a station' do
+    expect(page).to have_content("Most Bikes Available Count: #{Station.num_most_bikes_available}")
   end
 
-  it 'sees fewest bikes available at a station' do
+  xit 'sees the station that has the most bikes available' do
+    expect(page).to have_content("Station with Most Bikes Available: #{Station.highest_bike_count}")
   end
 
-  it 'sees the station that has the fewest bikes available' do
+  xit 'sees fewest bikes available at a station' do
+    expect(page).to have_content("Fewest Bikes Available Count: #{Station.num_least_bikes_available}")
   end
 
-  it 'sees the newest station' do
+  xit 'sees the station that has the fewest bikes available' do
+    expect(page).to have_content("Station with Fewest Bikes Available: #{Station.lowest_bike_count}")
   end
 
-  it 'sees the oldest station' do
+  xit 'sees the newest station' do
+    expect(page).to have_content("Newest Station: #{Station.newest}")
+  end
+
+  xit 'sees the oldest station' do
+    expect(page).to have_content("Oldest Station: #{Station.oldest}")
   end
 end
