b4a4d9af0d78faf04ca2346a3df849a94c230587
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-22 11:53:36 -0600

    feature spec framework for stations-dashboard for user

---
+++ spec/features/users/sees_stations_dashboard_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+# As a registered user,
+# When I visit '/stations-dashboard',
+# I see the Total count of stations,
+# I see the Average bikes available per station (based on docks),
+# I see the Most bikes available at a station (based on docks),
+# I see the Station(s) where the most bikes are available (based on docks),
+# I see the Fewest bikes available at a station (based on docks),
+# I see the Station(s) where the fewest bikes are available (based on docks),
+# I see the Most recently installed station,
+# I also see the Oldest station.
+
+describe 'visiting the /stations-dashboard' do
+  it 'sees a total count of stations' do
+  end
+
+  it 'sees average bikes available per station' do
+  end
+
+  it 'sees most bikes available at a station' do
+  end
+
+  it 'sees the station that has the most bikes available' do
+  end
+
+  it 'sees fewest bikes available at a station' do
+  end
+
+  it 'sees the station that has the fewest bikes available' do
+  end
+
+  it 'sees the newest station' do
+  end
+
+  it 'sees the oldest station' do
+  end
+end
