d7913b7fcd94083fe2edd79c88225c599f5d7037
JP Lynch <JPLynch35@gmail.com>
2018-07-22 11:54:36 -0600

    add test for seeing and clicking on Add to Cart

--- spec/features/visitors/sees_accessory_show_page_spec.rb
+++ spec/features/visitors/sees_accessory_show_page_spec.rb
@@ -2,13 +2,20 @@ require 'rails_helper'
 
 describe 'visits accessory show page' do
   it 'sees the image, title, description and price for the accessory' do
-    accessory1 = Accessory.create(image_url: 'www.insertrealpics.com', title: 'test1', description: 'this is a test1', price: 4, status: 1)
-    accessory2 = Accessory.create(image_url: 'www.insertrealpics.com', title: 'test2', description: 'this is a test2', price: 6, status: 1)
+    accessory = Accessory.create(image_url: 'www.insertrealpics.com', title: 'test1', description: 'this is a test1', price: 4, status: 1)
 
-    visit accessory_path(accessory1)
+    visit accessory_path(accessory)
 
-    expect(page).to have_content(accessory1.title)
-    expect(page).to have_content(accessory1.description)
-    expect(page).to have_content(accessory1.price)
+    expect(page).to have_content(accessory.title)
+    expect(page).to have_content(accessory.description)
+    expect(page).to have_content(accessory.price)
+  end
+  it 'can click a link for add to cart' do
+    accessory = Accessory.create(image_url: 'www.insertrealpics.com', title: 'test1', description: 'this is a test1', price: 4, status: 1)
+
+    visit accessory_path(accessory)
+
+    expect(page).to have_content('Add to Cart')
+    click_on 'Add to Cart'
   end
 end
