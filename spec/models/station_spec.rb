17718a23007c32761478fbdf121ee00b16328e65
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-22 12:32:04 -0600

    add class methods to station spec

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -7,4 +7,30 @@ describe Station, type: :model do
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:installation_date) }
   end
+
+  describe "class methods" do
+    it 'calculate total count of all stations'
+    end
+
+    it 'calculate average bikes per station'
+    end
+
+    it 'calculate most bikes available'
+    end
+
+    it 'calculate fewest bikes available'
+    end
+
+    it 'find station with most bikes available'
+    end
+
+    it 'find station with least bikes available'
+    end
+
+    it 'find oldest station'
+    end
+
+    it 'find newest station'
+    end
+  end
 end
