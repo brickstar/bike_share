d58c4527767b85a5861e1b87ed5367702b8e8f56
JP Lynch <JPLynch35@gmail.com>
2018-07-21 21:27:15 -0600

    add path for /bike-shop

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   get '/dashboard', to: 'dashboard#show'
+  get '/bike-shop', to: 'accessoriess#show'
 
   resources :users, only: [:new, :create]
 
