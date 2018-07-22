0173698c0e1d3b0440685281b808c8a17354e0b1
JP Lynch <JPLynch35@gmail.com>
2018-07-21 21:30:18 -0600

    add accessories controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   get '/dashboard', to: 'dashboard#show'
-  get '/bike-shop', to: 'accessoriess#show'
+  get '/bike-shop', to: 'accessories#index'
 
   resources :users, only: [:new, :create]
 
