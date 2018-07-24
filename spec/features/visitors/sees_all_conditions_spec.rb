b19e482043318fb4856c274c54bd62b9f2f8bc9e
brickstar <brickstar@gmail.com>
2018-07-23 23:41:58 -0600

    sketch of condition dashboard methods

--- spec/features/visitors/sees_all_conditions_spec.rb
+++ spec/features/visitors/sees_all_conditions_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'visiting the conditions index' do
   it 'sees a list of all conditions' do
     condition_1 = Condition.create(date: 'Tue, 04 Aug 2015', max_temp: 74.0, mean_temp: 68.0, min_temp: 61.0, mean_humidity: 61.0, mean_visibility: 58.0 , mean_wind_speed: 56.0, precipitation: 93.0)
-    condition_2 = Condition.create(date: 'Tue, 04 Aug 2015', max_temp: 71.0, mean_temp: 58.0, min_temp: 46.0, mean_humidity: 50.0, mean_visibility: 45.0 , mean_wind_speed: 41.0, precipitation: 93.0)
+    condition_2 = Condition.create(date: 'Tue, 05 Aug 2015', max_temp: 71.0, mean_temp: 58.0, min_temp: 46.0, mean_humidity: 50.0, mean_visibility: 45.0 , mean_wind_speed: 41.0, precipitation: 93.0)
 
     visit conditions_path
 
