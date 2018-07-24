f95d99508df251edd7281e8513cd79dbac8e1e72
brickstar <brickstar@gmail.com>
2018-07-23 23:41:03 -0600

    add custom one to many in condition model

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,4 +1,5 @@
 class Condition < ApplicationRecord
+  has_many :trips, class_name: 'Trip', primary_key: :date, foreign_key: :start_date
   validates_presence_of :max_temp,
                         :mean_temp,
                         :min_temp,
