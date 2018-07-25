fd68c3fe4c6b68dc4f3fb5998c4a21b5a672a8f8
brickstar <brickstar@gmail.com>
2018-07-24 22:38:52 -0600

    add admin conditions index delete spec

--- spec/features/admin/sees_conditions_index_spec.rb
+++ spec/features/admin/sees_conditions_index_spec.rb
@@ -5,10 +5,10 @@ describe 'admin user visits conditions index'do
     @admin = User.create(first_name: 'Pearl', last_name: 'Girl', street: '333 E 9th Ave', city: 'Denver', state: 'CO', zip_code: '12345', email: 'pearl@pearl.com', password: 'lovelove', role: 1)
     @condition_1 = Condition.create(date: 'Tue, 04 Aug 2015', max_temp: 74.0, mean_temp: 68.0, min_temp: 61.0, mean_humidity: 61.0, mean_visibility: 58.0 , mean_wind_speed: 56.0, precipitation: 93.0)
     @condition_2 = Condition.create(date: 'Tue, 04 Aug 2015', max_temp: 71.0, mean_temp: 58.0, min_temp: 46.0, mean_humidity: 50.0, mean_visibility: 45.0 , mean_wind_speed: 41.0, precipitation: 93.0)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
   end
 
   it 'sees all conditions and buttons to edit and delete each condition' do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
     visit conditions_path
 
@@ -38,5 +38,20 @@ describe 'admin user visits conditions index'do
     expect(page).to have_content(@condition_2.mean_visibility)
     expect(page).to have_content(@condition_2.mean_wind_speed)
     expect(page).to have_content(@condition_2.precipitation)
+  end
+
+  it 'can delete a condition' do
+
+    visit conditions_path
+
+    expect(page).to have_content(@condition_2.date)
+
+    within("#condition-#{@condition_2.id}") do
+      click_button('Delete')
+    end
+
+    expect(current_path).to eq(conditions_path)
+    expect(page).to_not have_content(@condition_2.date)
+    expect(page).to have_content("Successfully deleted condition ##{@condition.id}.")
   end
 end
