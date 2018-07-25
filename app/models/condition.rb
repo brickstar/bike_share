cdc6a48c9b572e5562e6f5be1b926bf24d18aa0f
brickstar <brickstar@gmail.com>
2018-07-24 19:00:33 -0600

    add relationship - commented out for now

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,12 +1,12 @@
 class Condition < ApplicationRecord
-  has_many :trips, class_name: 'Trip', primary_key: :date, foreign_key: :start_date
+  # has_many :trips, class_name: 'Trip', primary_key: :zip_code, foreign_key: :zip_code
   validates_presence_of :max_temp,
                         :mean_temp,
                         :min_temp,
                         :mean_humidity,
                         :mean_visibility,
                         :mean_wind_speed,
-                        :precipitation,
-                        :date
+                        :precipitation
+
   validates :date, uniqueness: true
 end
