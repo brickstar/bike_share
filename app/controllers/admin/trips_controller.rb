ce686e441a8c1ea39a9f9eec819edbf843dbabe0
JP Lynch <JPLynch35@gmail.com>
2018-07-25 15:37:30 -0600

    add tests and functionality for admin create trip

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -1,8 +1,33 @@
 class Admin::TripsController < Admin::BaseController
 
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.create(trip_params)
+    if @trip.save
+      flash[:successs] = "Trip ##{@trip.id} created."
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
   def destroy
     trip = Trip.destroy(params[:id])
     flash[:success] = "Successfully deleted trip ##{trip.id}."
     redirect_to trips_path
+  end
+
+  private
+  def initial_trip_params
+    params.require(:trip).permit(:duration, :start_date, :start_station_name, :end_date, :end_station_name, :bike_id, :subscription_type, :zip_code)
+  end
+  def trip_params
+    data_set = initial_trip_params
+    data_set[:start_station_id] = Station.find_by_name(initial_trip_params[:start_station_name]).id.to_s
+    data_set[:end_station_id] = Station.find_by_name(initial_trip_params[:end_station_name]).id.to_s
+    data_set
   end
 end
--- spec/features/admin/trip_new_spec.rb
+++ spec/features/admin/trip_new_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 describe 'admin user visits trip new' do
   describe 'fills in a form with trip attributes' do
     it 'should create a trip, land on show and see a flash success' do
+      Station.create(name: 'San Jose City Hall', city: 'San Jose', dock_count: 15, installation_date: 8/6/2013)
       admin = User.create(first_name: 'Pearl', last_name: 'Girl', street: '333 E 9th Ave', city: 'Denver', state: 'CO', zip_code: '12345', email: 'pearl@pearl.com', password: 'lovelove', role: 1)
       
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-      duration            = '234'
+      duration            = 234
       start_date          = Date.parse('12 March, 2018')
       start_station_name  = 'San Jose City Hall'
       end_date            = Date.parse('12 March, 2018')
       end_station_name    = 'San Jose City Hall'
+      bike_id             = 12
       subscription_type   = 'subscriber'
       zip_code            = '32165'
 
@@ -19,15 +21,16 @@ describe 'admin user visits trip new' do
 
       fill_in 'trip[duration]', with: duration
       fill_in 'trip[start_date]', with: start_date
-      fill_in 'trip[start_station_name]', with: start_station_name
+      select start_station_name, from: 'trip[start_station_name]'
       fill_in 'trip[end_date]', with: end_date
-      fill_in 'trip[end_station_name]', with: end_station_name
-      fill_in 'trip[subscription_type]', with: subscription_type
+      select end_station_name, from: 'trip[end_station_name]'
+      fill_in 'trip[bike_id]', with: bike_id
+      select subscription_type, from: 'trip[subscription_type]'
       fill_in 'trip[zip_code]', with: zip_code
 
       click_on 'Create Trip'
 
-      expect(current_path).to eq(condition_path(Trip.last))
+      expect(current_path).to eq(trip_path(Trip.last))
       expect(page).to have_content(duration)
       expect(page).to have_content(start_date)
       expect(page).to have_content(start_station_name)
