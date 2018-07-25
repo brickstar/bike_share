6573b4f99b6308fe377f96deaff0a9f20f7af98c
brickstar <brickstar@gmail.com>
2018-07-25 15:37:07 -0600

    replace image url in tests

--- spec/features/users/checkout_workflow_spec.rb
+++ spec/features/users/checkout_workflow_spec.rb
@@ -4,8 +4,8 @@ describe 'a user accessing the cart show page' do
   before :each do
     @user = User.create(first_name: 'Bob', last_name: 'Santos', street: '123 Main Street', city: 'Cranford', state: 'NJ', zip_code: '07016', email: 'Bob@gmail.com', password: 'secretsecret')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-    @accessory1 = Accessory.create(image_url: 'www.insertrealpics.com', title: 'test1', description: 'this is a test', price: 4)
-    @accessory2 = Accessory.create(image_url: 'www.insertrealpics.com', title: 'test2', description: 'this is a test', price: 4)
+    @accessory1 = Accessory.create(image_url: 'https://robohash.org/1', title: 'test1', description: 'this is a test', price: 4)
+    @accessory2 = Accessory.create(image_url: 'https://robohash.org/1', title: 'test2', description: 'this is a test', price: 4)
     @cart = Cart.new({
       '1' => 2,
       '2' => 3
