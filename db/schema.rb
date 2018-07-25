b22c32b3e458cc1f58cd22d9e52bb59f445dbf8f
brickstar <brickstar@gmail.com>
2018-07-24 19:02:06 -0600

    add zipcode to conditions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180722031933) do
+ActiveRecord::Schema.define(version: 20180724204527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180722031933) do
     t.float "precipitation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zip_code"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
