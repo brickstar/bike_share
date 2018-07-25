bacb165fadb531d0a598f82e5a7ea287d55ae72c
JP Lynch <JPLynch35@gmail.com>
2018-07-24 20:54:11 -0600

    add feature test for admin to see edit and delete buttons on trip index

---
+++ spec/features/admin/trip_index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'visiting trip index page can see all trips with attributes' do
+  before :each do
+    @admin = User.create(first_name: 'Boss',last_name: 'Lady', street: '123 Main St', city: 'Denver', state: 'CO', zip_code: '80304', email: 'Jeff@Turing.com', password: 'password', role: 1 )
+    @trip1 = Trip.create(duration: 120, start_date: DateTime.strptime('10/12/2015 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'Test Station 2', start_station_id: 2, end_date: DateTime.strptime('10/12/2015 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'Test Station 2', end_station_id: 2, bike_id: 2, subscription_type: 'subscriber', zip_code: '32174')
+    @trip2 = Trip.create(duration: 130, start_date: DateTime.strptime('10/13/2015 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'Test Station 1', start_station_id: 1, end_date: DateTime.strptime('10/13/2015 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'Test Station 3', end_station_id: 3, bike_id: 2, subscription_type: 'customer', zip_code: '32174')
+    @trip3 = Trip.create(duration: 140, start_date: DateTime.strptime('07/14/2016 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'Test Station 1', start_station_id: 1, end_date: DateTime.strptime('07/14/2016 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'Test Station 3', end_station_id: 3, bike_id: 2, subscription_type: 'customer', zip_code: '32174')
+    @trip4 = Trip.create(duration: 150, start_date: DateTime.strptime('09/12/2016 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'Test Station 1', start_station_id: 1, end_date: DateTime.strptime('09/12/2016 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'Test Station 3', end_station_id: 3, bike_id: 3, subscription_type: 'customer', zip_code: '32174')
+  end
+  it 'can see edit and delete buttons alongside each trip' do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    visit trips_path
+
+    expect(current_path).to eq(trips_path)
+    expect(page).to have_content(@trip1.id)
+    expect(page).to have_content(@trip2.id)
+    expect(page).to have_content(@trip3.id)
+    expect(page).to have_content(@trip4.id)
+    expect(page).to have_content(@trip1.duration)
+    expect(page).to have_content(@trip1.start_date)
+    expect(page).to have_content(@trip1.start_station_name)
+    expect(page).to have_content(@trip1.start_station_id)
+    expect(page).to have_content(@trip1.end_date)
+    expect(page).to have_content(@trip1.end_station_name)
+    expect(page).to have_content(@trip1.end_station_id)
+    expect(page).to have_content(@trip1.bike_id)
+    expect(page).to have_content(@trip1.subscription_type)
+    expect(page).to have_content(@trip1.zip_code)
+    expect(page).to have_button('Edit')
+    expect(page).to have_button('Delete')
+  end
+end
