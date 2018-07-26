7cbf43ae0e596523d99d88d464e0a9a2cd23f8ed
brickstar <brickstar@gmail.com>
2018-07-25 18:03:16 -0600

    add condition id in trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180724204527) do
+ActiveRecord::Schema.define(version: 20180725225621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 20180724204527) do
     t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "condition_id"
+    t.index ["condition_id"], name: "index_trips_on_condition_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -88,4 +90,5 @@ ActiveRecord::Schema.define(version: 20180724204527) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "trips", "conditions"
 end
