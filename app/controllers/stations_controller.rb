568989b55c0fb78810028a94a78c5f1537c433fd
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-26 15:35:14 -0600

    finish complete build out for user station show to include instance analysis

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -5,14 +5,6 @@ class StationsController < ApplicationController
   end
 
   def show
-    @station = Station.friendly.find(params[:id]).includes(
-                                                            @total_trips_ended = Station.total_trips_ended,
-                                                            @total_trips_started = Station.total_trips_started,
-                                                            @freq_destination = Station.most_riders_went_to,
-                                                            @freq_origination = Station.most_riders_came_from,
-                                                            @highest_volume = Station.highest_start_volume_date,
-                                                            @most_users_from = Station.most_users_from_zipcode,
-                                                            @most_started_bike = Station.most_started_bike_id,
-                                                          )
+    @station = Station.friendly.find(params[:id])
   end
 end
