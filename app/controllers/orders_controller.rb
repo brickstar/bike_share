042531714b670cfc8c90ea7e149ae54fe873988d
JP Lynch <JPLynch35@gmail.com>
2018-07-26 14:55:01 -0600

    add cart create path in checkout button

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,10 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  def create
+    order = current_user.orders.create(status: 'ordered')
+    
+    redirect_to dashboard_path
+  end
 end
