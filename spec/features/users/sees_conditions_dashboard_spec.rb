9ce387e05688619521edb244883e709263210f72
Emmie Hayes <32617260+emmiehayes@users.noreply.github.com>
2018-07-25 19:02:54 -0600

    removed comments

--- spec/features/users/sees_conditions_dashboard_spec.rb
+++ spec/features/users/sees_conditions_dashboard_spec.rb
@@ -1,44 +1,5 @@
 require 'rails_helper'
 
-=begin
-
-As a registered user,
-When I visit '/conditions-dashboard',
-I see the Breakout of
-  average number of rides,
-  highest number of rides,
-  and lowest number of rides
-  on days with a high temperature
-  in 10 degree chunks (e.g. average number of rides on days with
-  high temps between fifty and sixty degrees
-
-  44-102
-
-  7 total
-
-  Condition.select("conditions.max_temp, count(trips.id) as trip_count")
-  .joins("inner join trips on conditions.zip_code=trips.zip_code")
-  .where("conditions.date=trips.start_date")
-  .group(:max_temp,:id)
-
-
-  Condition.joins("INNER JOIN trips ON conditions.date = trips.start_date")
-  .select("conditions.date, count(trips.id) as trip_count")
-  .where(max_temp: 40..50)
-  .group("conditions.date")
-  .order("trip_count DESC")
-  .first.trip_count
-
-  def thing
-    ary = [[40, 50], [50, 60], [60, 70], [70, 80], [80, 90], [90, 100], [100, 110]]
-    thing = ary.map do |array|
-      Condition.select("conditions.id, conditions.date").where("max_temp >= ? AND max_temp <= ?", array[0][0], array[0][1]).pluck(:date)
-    end
-    thing
-  end
-
-=end
-
 describe 'registered user visits /conditions-dashboard' do
   describe 'on days with high temperature in 10 degree chunks' do
     it 'should see highest number of rides' do
@@ -49,40 +10,3 @@ describe 'registered user visits /conditions-dashboard' do
     end
   end
 end
-
-=begin
-
-I see the Breakout of
- average number of rides,
- highest number of rides,
- and lowest number of rides
- on days with precipitation in half-inch increments,
-
- T-0-3.36
-
- 8 total
-=end
-
-=begin
-I see the Breakout of
-  average number of rides,
-  highest number of rides, and
-  lowest number of rides
-  on days with mean wind speeds in four mile increments,
-
-  0-23
-
-  6 total
-=end
-
-=begin
-I see the Breakout of
-  average number of rides,
-  highest number of rides, and
-  lowest number of rides
-  on days with mean visibility in miles in four mile increments.
-
-  4-20
-
-  5 total
-=end
