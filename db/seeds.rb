ebf41d80039bce1265434882d02d58ae6e62490d
brickstar <brickstar@gmail.com>
2018-07-25 18:03:36 -0600

    add condition id to trips

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,24 @@ CSV.foreach('./../sf-bay-area-bike-share/trip.csv', headers: true, header_conver
     next if row[:zip_code].length != 5
     formatted_start_date = Date.strptime row[2], '%m/%d/%Y'
     formatted_end_date = Date.strptime row[2], '%m/%d/%Y'
-    Trip.create(id:row[0], duration:row[1], start_date:formatted_start_date, start_station_name:row[3], start_station_id:row[4], end_date:formatted_end_date, end_station_name:row[6], end_station_id:row[7], bike_id:row[8], subscription_type:row[9], zip_code:row[10])
+    condition = Condition.find_by(date: formatted_end_date)
+    # if condition
+    #   condition_id = condition.id
+    # else
+    #   condition_id = nil
+    # end
+    Trip.create(id: row[0],
+                duration: row[1],
+                start_date: formatted_start_date,
+                start_station_name: row[3],
+                start_station_id: row[4],
+                end_date: formatted_end_date,
+                end_station_name: row[6],
+                end_station_id: row[7],
+                bike_id: row[8],
+                subscription_type: row[9],
+                condition_id: condition.try(:id),
+                zip_code: row[10])
   end
 end
 
