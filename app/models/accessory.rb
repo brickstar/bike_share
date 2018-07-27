f9e8521c2a596752ae85caa00c0541d4088369bc
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-26 18:30:53 -0600

    update accessory model validations and test

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,5 +1,12 @@
 class Accessory < ApplicationRecord
-  validates_presence_of :image_url, :title, :description, :price, :status
+
+  validates_uniqueness_of :title, presence: true
+  validates_presence_of :description
+  validates_numericality_of :price, greater_than: 0
+  validates_presence_of :image_url, optional: true
+  validates_presence_of :status
+
   enum status: ['retired', 'active']
+
   paginates_per 12
 end
--- spec/models/accessory_spec.rb
+++ spec/models/accessory_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 describe Accessory, type: :model do
   describe 'validations' do
-    it {should validate_presence_of(:image_url)}
-    it {should validate_presence_of(:title)}
+    it {should validate_uniqueness_of(:title)}
     it {should validate_presence_of(:description)}
-    it {should validate_presence_of(:price)}
     it {should validate_presence_of(:status)}
+    it {should validate_presence_of(:image_url)}
+    it {should validate_numericality_of(:price).
+                  is_greater_than(0)}
   end
 end
