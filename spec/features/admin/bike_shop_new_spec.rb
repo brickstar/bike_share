a37d5add604e5e133e02233a61f27f811a1c5b30
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-27 16:45:00 -0600

    complete create new accessory for admin

--- spec/features/admin/bike_shop_new_spec.rb
+++ spec/features/admin/bike_shop_new_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
-=begin
-As an admin user,
-When I visit admin bikeshop new
-I can create an accessory,
-The photo is optional. If not present, a stand-in photo is used. (PAPERCLIP)
-=end
-
 describe 'visits bike shop' do
   before :each do
     @admin = User.create(first_name: 'Jeff', last_name: 'Casimir', street: '123 Main St', city: 'Denver', state: 'Colorado', zip_code: '80403', email: 'jeff@turing.com', password: 'password', role: 1)
@@ -17,10 +10,11 @@ describe 'visits bike shop' do
     new_accessory_title = 'Testing Setup Accessory'
     new_accessory_description = 'Testing Description'
     new_accessory_price = 10.00
-    new_image_url = 'new image url'
+    new_image_url = "https://robohash.org/1"
     new_status = 'active'
 
     visit accessories_path
+
     expect(page).to have_button('Create New Accessory')
 
     click_button 'Create New Accessory'
@@ -35,10 +29,9 @@ describe 'visits bike shop' do
 
     expect(current_path).to eq(accessory_path(Accessory.last))
 
-    click_on 'Next'
-
-    expect(page).to have_content(@accessory1.title)
-    expect(page).to have_content(@accessory1.description)
-    expect(page).to have_content(@accessory1.price)
+    expect(page).to have_content(new_accessory_title)
+    expect(page).to have_content(new_accessory_description)
+    expect(page).to have_content(new_accessory_price)
+    expect(page).to have_content(new_status)
   end
 end
