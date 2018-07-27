b9285c72c55ecb661588ecb5754a84eb3d5e003f
brickstar <brickstar@gmail.com>
2018-07-27 16:18:18 -0600

    changed accessory price to integer from float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180726175632) do
+ActiveRecord::Schema.define(version: 20180727190015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180726175632) do
     t.string "image_url"
     t.string "title"
     t.string "description"
-    t.float "price"
+    t.integer "price"
     t.integer "status", default: 1
   end
 
