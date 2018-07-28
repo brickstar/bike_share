dd456fc6999296f2c4d211a363c01108b0d40955
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-28 12:12:31 -0600

    seed data, build out query params, create dashboard index and pass feature test

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,15 @@
 class Admin::DashboardController < Admin::BaseController
 
   def show
-    @orders = Order.all
-    @paid = Order.amount_by_status('paid')
-    @completed = Order.amount_by_status('completed')
-    @cancelled = Order.amount_by_status('cancelled')
-    @ordered = Order.amount_by_status('ordered')
+    @paid_status = Order.amount_by_status('paid')
+    @completed_status = Order.amount_by_status('completed')
+    @cancelled_status = Order.amount_by_status('cancelled')
+    @ordered_status = Order.amount_by_status('ordered')
+    if params[:status]
+      @orders = Order.where(status: params['status'])
+        render :index
+    else
+      @orders = Order.all
+    end
   end
-
 end
