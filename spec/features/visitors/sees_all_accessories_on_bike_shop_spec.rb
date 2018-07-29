0fa9bc1d55f7b2ec393758ac3f5127f5f7f13bca
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-28 18:10:48 -0600

    clean up visitor testing

--- spec/features/visitors/sees_all_accessories_on_bike_shop_spec.rb
+++ spec/features/visitors/sees_all_accessories_on_bike_shop_spec.rb
@@ -16,6 +16,7 @@ describe 'visits bike shop' do
     @accessory12 = Accessory.create(image_url: 'https://robohash.org/1', title: 'test12', description: 'this is a test', price: 4)
     @accessory13 = Accessory.create(image_url: 'https://robohash.org/1', title: 'test13', description: 'this is a test', price: 4)
   end
+
   it 'sees at least 12 bike accessories' do
     visit accessories_path
 
@@ -23,6 +24,7 @@ describe 'visits bike shop' do
     within("#accessory_#{@accessory1.id}") do
       expect(page).to have_button('Add to Cart')
     end
+
     expect(page).to have_content(@accessory2.title)
     expect(page).to have_content(@accessory3.title)
     expect(page).to have_content(@accessory4.title)
@@ -33,17 +35,21 @@ describe 'visits bike shop' do
     expect(page).to have_content(@accessory9.title)
     expect(page).to have_content(@accessory10.title)
     expect(page).to have_content(@accessory11.title)
+
     within("#accessory_#{@accessory11.id}") do
       expect(page).to have_button('Add to Cart')
     end
+
     expect(page).to have_content(@accessory12.title)
     expect(page).to_not have_content(@accessory13.title)
   end
+
   it 'sees a button to click on and see more trips' do
     visit accessories_path
 
     expect(page).to have_content('Next ›')
   end
+
   it 'can go forward and back in bike-shop on second bike-shop page' do
     visit accessories_path
     click_on 'Next'
@@ -57,17 +63,20 @@ describe 'visits bike shop' do
     expect(page).to have_content(@accessory12.title)
     expect(page).to_not have_content(@accessory13.title)
   end
+
   it 'sees Accessory Retired instead of Add to Cart for retired accessories' do
     visit accessories_path
 
     within("#accessory_#{@accessory4.id}") do
       expect(page).to have_button('Add to Cart')
     end
+
     within("#accessory_#{@accessory5.id}") do
       expect(page).to_not have_button('Add to Cart')
       expect(page).to have_content('Accessory Retired')
     end
   end
+
   it 'can link to accessory show' do
     visit accessories_path
 
--- spec/features/visitors/sees_attributes_on_trip_show_spec.rb
+++ spec/features/visitors/sees_attributes_on_trip_show_spec.rb
@@ -16,8 +16,10 @@ describe 'visiting the trip show page' do
     expect(page).to have_content(trip.subscription_type)
     expect(page).to have_content(trip.zip_code)
   end
+
   it 'does not see admin edit and delete buttons' do
     trip = Trip.create(duration: 400, start_date: '12/12/2015 12:12', start_station_name: 'Test Station 1', start_station_id: 1, end_date: '12/12/2015 12:42', end_station_name: 'Test Station 2', end_station_id: 2, bike_id: 2, subscription_type: 'customer', zip_code: '32174')
+
     visit trip_path(trip)
 
     expect(page).to_not have_button('Edit')
