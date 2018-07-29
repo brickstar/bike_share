d91fcb5957f1e746c3923b5c9c9854da787bd2ec
brickstar <brickstar@gmail.com>
2018-07-29 14:07:05 -0600

    sketch

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -2,13 +2,13 @@ class Condition < ApplicationRecord
   # has_many :trips, class_name: 'Trip', primary_key: :zip_code, foreign_key: :zip_code
   has_many :trips
   validates_presence_of :max_temp,
-                        :mean_temp,
-                        :min_temp,
-                        :mean_humidity,
-                        :mean_visibility,
-                        :mean_wind_speed,
-                        :precipitation,
-                        :date
+  :mean_temp,
+  :min_temp,
+  :mean_humidity,
+  :mean_visibility,
+  :mean_wind_speed,
+  :precipitation,
+  :date
 
   validates :date, uniqueness: true
 
@@ -28,5 +28,53 @@ class Condition < ApplicationRecord
 
   def self.least_rides
     find_by(date: Trip.max_min_rides.last.date_mod)
+  end
+
+  def self.temp_ranges
+    range = []
+    min = minimum(:max_temp).floor(-1)
+    max = maximum(:max_temp).ceil(-1)
+    until min >= max
+      range << [min, (min + 9.9)]
+      min += 10
+    end
+    range
+  end
+
+  def self.temp_all_rides
+    temp_ranges.map do |r|
+      joins(:trips)
+      .where("max_temp >= #{r[0]} AND max_temp <= #{r[1]}")
+      .group(:condition_id)
+      .count(:condition_id)
+      .values
+    end
+  end
+
+2  
+  def precipitation_range
+    [[0.0, 0.49]]
+  end
+
+  def self.mean_wind_speed_ranges
+    range = []
+    min = minimum(:mean_wind_speed)
+    max = maximum(:mean_wind_speed)
+    until min >= max
+      range << [min, (min + 3.9)]
+      min += 4
+    end
+    range
+  end
+
+  def self.mean_visibility_ranges
+    range = []
+    min = minimum(:mean_visibility)
+    max = maximum(:mean_visibility)
+    until min >= max
+      range << [min, (min + 3.9)]
+      min += 4
+    end
+    range
   end
 end
--- spec/features/users/sees_conditions_dashboard_spec.rb
+++ spec/features/users/sees_conditions_dashboard_spec.rb
@@ -65,21 +65,56 @@ end
 thing
 end
 
-ary = [[0.0, 0.5], [0.5, 1.0], [1.0, 70], [70, 80], [80, 90], [90, 100], [100, 110]]
+ary = [[0, 0.5], [0.5, 1], [1.0, 70], [70, 80], [80, 90], [90, 100], [100, 110]]
 
+Condition.joins(:trips).where("max_temp >= #{r[0]} AND max_temp <= #{r[1]}")
+.group(:condition_id)
+.count(:condition_id)
+.values
 =end
 
 describe 'registered user visits /conditions-dashboard' do
+  before(:each) do
+    @user = User.create(first_name: 'Pearl', last_name: 'Girl', street: '333 E 9th Ave', city: 'Denver', state: 'CO', zip_code: '12345', email: 'pearl@pearl.com', password: 'lovelove', role: 1)
+    @condition_1 = Condition.create(date: 'Tue, 04 Aug 2015', max_temp: 51, mean_temp: 68, min_temp: 61, mean_humidity: 61, mean_visibility:  1, mean_wind_speed: 1, precipitation: 0.1)
+    @condition_2 = Condition.create(date: 'Wed, 05 Jan 2013', max_temp: 59, mean_temp: 58, min_temp: 46, mean_humidity: 50, mean_visibility: 3, mean_wind_speed: 3, precipitation: 0.4)
+    @condition_3 = Condition.create(date: 'Wed, 06 Jan 2013', max_temp: 61, mean_temp: 58, min_temp: 46, mean_humidity: 50, mean_visibility: 5, mean_wind_speed: 5, precipitation: 0.6)
+    @condition_4 = Condition.create(date: 'Wed, 07 Jan 2013', max_temp: 69, mean_temp: 58, min_temp: 46, mean_humidity: 50, mean_visibility: 8, mean_wind_speed: 8, precipitation: 0.9)
+    @trip_1 = @condition_1.trips.create(duration: 120, start_date: DateTime.strptime('08/04/2015 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'San Jose City Hall', start_station_id: 1, end_date: DateTime.strptime('08/04/2015 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'San Jose City Hall', end_station_id: 1, bike_id: 1, subscription_type: 'subscriber', zip_code: '32174')
+    @trip_2 = @condition_2.trips.create(duration: 120, start_date: DateTime.strptime('01/05/2013 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'San Jose City Hall', start_station_id: 1, end_date: DateTime.strptime('01/05/2013 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'San Jose City Hall', end_station_id: 1, bike_id: 1, subscription_type: 'subscriber', zip_code: '32174')
+    @trip_3 = @condition_2.trips.create(duration: 120, start_date: DateTime.strptime('01/05/2013 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'San Jose City Hall', start_station_id: 1, end_date: DateTime.strptime('01/05/2013 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'San Jose City Hall', end_station_id: 1, bike_id: 1, subscription_type: 'subscriber', zip_code: '32174')
+    @trip_4 = @condition_2.trips.create(duration: 120, start_date: DateTime.strptime('01/05/2013 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'San Jose City Hall', start_station_id: 1, end_date: DateTime.strptime('01/05/2013 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'San Jose City Hall', end_station_id: 1, bike_id: 1, subscription_type: 'subscriber', zip_code: '32174')
+    @trip_5 = @condition_3.trips.create(duration: 120, start_date: DateTime.strptime('01/06/2013 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'San Jose City Hall', start_station_id: 1, end_date: DateTime.strptime('01/06/2013 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'San Jose City Hall', end_station_id: 1, bike_id: 1, subscription_type: 'subscriber', zip_code: '32174')
+    @trip_6 = @condition_4.trips.create(duration: 120, start_date: DateTime.strptime('01/07/2013 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'San Jose City Hall', start_station_id: 1, end_date: DateTime.strptime('01/07/2013 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'San Jose City Hall', end_station_id: 1, bike_id: 1, subscription_type: 'subscriber', zip_code: '32174')
+    @trip_7 = @condition_4.trips.create(duration: 120, start_date: DateTime.strptime('01/07/2013 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'San Jose City Hall', start_station_id: 1, end_date: DateTime.strptime('01/07/2013 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'San Jose City Hall', end_station_id: 1, bike_id: 1, subscription_type: 'subscriber', zip_code: '32174')
+    @trip_8 = @condition_4.trips.create(duration: 120, start_date: DateTime.strptime('01/07/2013 12:12', '%m/%d/%Y %H:%M'), start_station_name: 'San Jose City Hall', start_station_id: 1, end_date: DateTime.strptime('01/07/2013 12:42', '%m/%d/%Y %H:%M'), end_station_name: 'San Jose City Hall', end_station_id: 1, bike_id: 1, subscription_type: 'subscriber', zip_code: '32174')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit conditions_dashboard_path
+  end
   describe 'on days with high temperature in 10 degree chunks' do
-    it 'should see highest number of rides' do
+    xit 'should see highest number of rides' do
+      visit conditions_dashboard_path
+
+      expect(page).to have_content('Highest rides by maximum temperature:')
+      expect(page).to have_content('50-59.9: 3')
+      expect(page).to have_content('60-69.9: 3')
     end
-    it 'should see lowest number of rides' do
+    xit 'should see lowest number of rides' do
+      visit conditions_dashboard_path
+
+      expect(page).to have_content('Lowest rides by maximum temperature:')
+      expect(page).to have_content('50-59.9: 1')
+      expect(page).to have_content('60-69.9: 1')
     end
-    it 'should see average number of rides' do
+    xit 'should see average number of rides' do
+      visit conditions_dashboard_path
+
+      expect(page).to have_content('Average rides by maximum temperature:')
+      expect(page).to have_content('50-59.9: 2')
+      expect(page).to have_content('60-69.9: 2')
     end
   end
 end
-
 =begin
 
 I see the Breakout of
