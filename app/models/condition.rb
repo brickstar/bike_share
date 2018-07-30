2d812066b5a73b39425efc1402508c802443a43f
brickstar <brickstar@gmail.com>
2018-07-30 09:05:07 -0600

    ranges

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -30,15 +30,40 @@ class Condition < ApplicationRecord
     find_by(date: Trip.max_min_rides.last.date_mod)
   end
 
-  def self.temp_ranges
-    range = []
+  def self.float_range_finder(step, min, max)
+    min = (min * 2).floor / 2.to_f
+    max = (max * 2).ceil / 2.to_f
+    thing = (min..max).step(step).to_a
+  end
+
+  def self.precipitation_range
+    min = minimum(:precipitation)
+    max = maximum(:precipitation)
+    float_range_finder(0.5, min, max)
+  end
+
+  def self.max_temp_range
     min = minimum(:max_temp).floor(-1)
     max = maximum(:max_temp).ceil(-1)
-    until min >= max
-      range << [min, (min + 9.9)]
-      min += 10
-    end
-    range
+    integer_range_finder(10, min, max)
+  end
+
+  def self.visibility_range
+    min = minimum(:mean_visibility)
+    max = maximum(:mean_visibility)
+    integer_range_finder(4, min, max)
+  end
+
+  def self.wind_range
+    min = minimum(:mean_wind_speed)
+    max = maximum(:mean_wind_speed)
+    integer_range_finder(4, min, max)
+  end
+
+  def self.integer_range_finder(step, min, max)
+    remainder = max % step
+    max = max + (step - remainder) if remainder != 0
+    (min..max).step(step).to_a
   end
 
   def self.temp_all_rides
@@ -49,11 +74,6 @@ class Condition < ApplicationRecord
       .count(:condition_id)
       .values
     end
-  end
-
-2  
-  def precipitation_range
-    [[0.0, 0.49]]
   end
 
   def self.mean_wind_speed_ranges
